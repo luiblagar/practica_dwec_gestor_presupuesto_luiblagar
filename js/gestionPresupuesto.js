04e6230bc47bdbf972423e4ebd3b2d70bf7c8603
Luis Blanco <luiblagar@alu.edu.gva.es>
2024-10-22 23:51:33 +0200

    Fundamentos de JS II: Funcion calcularTotalGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -110,11 +110,15 @@ function borrarGasto(id) {
 }
 
 function calcularTotalGastos() {
-
+    let total=0;
+    for(let gasto of gastos){
+        total+=gasto.valor;
+    }
+    return total;
 }
 
 function calcularBalance() {
-
+    
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
