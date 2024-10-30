ce5b7a42d2fa82427a623e1be66153673b639145
Luis Blanco <luiblagar@alu.edu.gva.es>
2024-10-30 03:11:00 +0100

    Interacción con HTML: arreglada importación y creación de gastos, pasa el primer test

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,7 +1,6 @@
-import * as gestionPresupuesto from "./gestionPresupuesto";
-import * as gestionPresupuestoWeb from "./gestionPresupuestoWeb";
+import * as gestionPresupuesto from "./gestionPresupuesto.js";
+import * as gestionPresupuestoWeb from "./gestionPresupuestoWeb.js";
 
-// TODO 
 // Actualizar el presupuesto a 1500€ (función actualizarPresupuesto)
 console.log("Acualiza presupuesto");
 gestionPresupuesto.actualizarPresupuesto(1500);
@@ -12,17 +11,17 @@ gestionPresupuestoWeb.mostrarDatoEnId("div#presupuesto", gestionPresupuesto.most
 // Crear los siguientes gastos (función crearGasto):
 let gastosCreados = [];
 // ("Compra carne", 23.44, "2021-10-06", "casa", "comida")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
 // ("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
 // ("Bonobús", 18.60, "2020-05-26", "transporte")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
 // ("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
 // ("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
 // ("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
-gastosCreados.push(gestionPresupuesto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
+gastosCreados.push(new gestionPresupuesto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
 
 // Añadir los gastos creados (función anyadirGasto)
 for (let gasto of gastosCreados) {
@@ -35,6 +34,7 @@ gestionPresupuestoWeb.mostrarDatoEnId("div#gastos-totales", gestionPresupuesto.c
 // Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId)
 gestionPresupuestoWeb.mostrarDatoEnId("div#balance-total", gestionPresupuesto.calcularBalance());
 
+// TODO
 // Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
 // Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1 (funciones filtrarGastos y mostrarGastoWeb)
 // Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2 (funciones filtrarGastos y mostrarGastoWeb)
